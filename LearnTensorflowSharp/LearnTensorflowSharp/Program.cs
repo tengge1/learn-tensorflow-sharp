b193a765ac6e239a9bcf8eaa544f218a3f77cf55
Li Teng <930372551@qq.com>
2017-08-31 21:44:15 +0800

    source

---
+++ LearnTensorflowSharp/LearnTensorflowSharp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TensorFlow;
+
+namespace LearnTensorflowSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var sess = new TFSession())
+            {
+                var graph = sess.Graph;
+                var a = graph.Const(3);
+                var b = graph.Const(4);
+
+                var result = sess.GetRunner().Run(graph.Add(a, b));
+                Console.WriteLine(result.GetValue());
+            }
+        }
+    }
+}
