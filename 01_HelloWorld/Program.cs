5e27343ad8b57ba6f8b74c6440cbbf6175abd7bf
liteng <930372551@qq.com>
2017-11-15 20:52:01 +0800

    01 Hello World

---
+++ 01_HelloWorld/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TensorFlow;
+
+namespace _01_HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var session = new TFSession())
+            {
+                var graph = session.Graph;
+                var a = graph.Const(2);
+                var b = graph.Const(3);
+                var result = session.GetRunner().Run(graph.Add(a, b)).GetValue();
+                Console.WriteLine("a+b={0}", result);
+            }
+        }
+    }
+}
