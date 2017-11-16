fd7b6bfebe378e9dbea63f0e5f3d9f88c825eee8
liteng <930372551@qq.com>
2017-11-16 21:38:15 +0800

    p02_UseVariable

---
+++ p02_UseVariable/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TensorFlow;
+
+namespace p02_UseVariable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var session = new TFSession();
+            var graph = session.Graph;
+
+            var a = graph.Placeholder(TFDataType.Float, operName: "a");
+            var b = graph.Placeholder(TFDataType.Float, operName: "b");
+            var c = graph.Placeholder(TFDataType.Float, operName: "c");
+
+            var d = graph.Add(a, b);
+            var e = graph.Mul(d, c);
+            var f = graph.Pow(e, b);
+            var g = graph.Div(f, a);
+            var h = graph.Sqrt(g);
+
+            // sqrt(((1+2)*3)^2/a) = 9
+            var result = session.GetRunner()
+                .AddInput("a", 1.0f)
+                .AddInput("b", 2.0f)
+                .AddInput("c", 3.0f)
+                .Run(h).GetValue();
+
+            Console.WriteLine("h={0}", result);
+
+            session.CloseSession();
+        }
+    }
+}
