cc0a4d6e6fae11d2dcefa5a54c7887eeea2a8092
liteng <930372551@qq.com>
2017-11-16 20:43:39 +0800

    01 hello,world

--- 01_HelloWorld/Program.cs
+++ 01_HelloWorld/Program.cs
@@ -16,8 +16,13 @@ namespace _01_HelloWorld
                 var graph = session.Graph;
                 var a = graph.Const(2);
                 var b = graph.Const(3);
-                var result = session.GetRunner().Run(graph.Add(a, b)).GetValue();
-                Console.WriteLine("a+b={0}", result);
+                Console.WriteLine("a=2,b=3");
+
+                var addResult = session.GetRunner().Run(graph.Add(a, b)).GetValue();
+                Console.WriteLine("a+b={0}", addResult);
+
+                var multiplyResult = session.GetRunner().Run(graph.Mul(a, b)).GetValue();
+                Console.WriteLine("a*b={0}", multiplyResult);
             }
         }
     }
