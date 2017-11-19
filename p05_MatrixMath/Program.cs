7519a3e92dad3d4e117375c59ea01f643d9a390e
liteng <930372551@qq.com>
2017-11-19 11:27:55 +0800

    p05_MatrixMath

---
+++ p05_MatrixMath/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TensorFlow;
+
+namespace p05_MatrixMath
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var g = new TFGraph();
+
+            var a = g.Const(new double[,] { { 1, 2 } });
+            var b = g.Const(new double[,] { { 3 }, { 4 } });
+
+            var mul = g.MatMul(a, b);
+
+            var sess = new TFSession(g);
+
+            var result = sess.GetRunner().Run(mul);
+
+            Console.WriteLine(result.ToString());
+            Console.WriteLine(((double[,])result.GetValue())[0, 0]);
+        }
+    }
+}
