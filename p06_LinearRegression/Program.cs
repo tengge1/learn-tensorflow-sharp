c6af1882ffdd1f1701f20dda237e422a770d4d2c
liteng <930372551@qq.com>
2017-11-19 15:48:49 +0800

    p07_MNIST

---
+++ p06_LinearRegression/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TensorFlow;
+
+namespace p03_LinearFit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var xData = new double[] {
+                3.3, 4.4, 5.5, 6.71, 6.93, 4.168, 9.779, 6.182, 7.59, 2.167,
+                7.042, 10.791, 5.313, 7.997, 5.654, 9.27, 3.1
+            };
+            var yData = new double[] {
+                1.7,2.76,2.09,3.19,1.694,1.573,3.366,2.596,2.53,1.221,
+                 2.827,3.465,1.65,2.904,2.42,2.94,1.3
+            };
+
+            var g = new TFGraph();
+
+            var x = g.Placeholder(TFDataType.Float);
+            var y = g.Placeholder(TFDataType.Float);
+
+            var ran = new Random();
+            var w = g.Variable(g.Const((float)ran.Next()), operName: "weight");
+            var b = g.Variable(g.Const((float)ran.Next()), operName: "bias");
+            var output = g.Add(g.Mul(x, w.Read), b.Read);
+
+            var loss = g.ReduceSum(g.Pow(g.Sub(output, y), g.Const(2.0)));
+
+            var sess = new TFSession(g);
+
+            for (var i = 0; i < 100; i++)
+            {
+
+            }
+        }
+    }
+}
---
+++ p07_MNIST/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TensorFlow;
+using Learn.Mnist;
+
+namespace p07_MNIST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mnist = Mnist.Load();
+
+            var trainCount = 5000;
+            var testCount = 200;
+
+            float[,] trainingImages, trainingLabels, testImages, testLabels;
+            mnist.GetTrainReader().NextBatch(trainCount, out trainingImages, out trainingLabels);
+            mnist.GetTestReader().NextBatch(testCount, out testImages, out testLabels);
+
+            var g = new TFGraph();
+
+            var trainingInput = g.Placeholder(TFDataType.Float, new TFShape(-1, 784));
+            var xte = g.Placeholder(TFDataType.Float, new TFShape(784));
+
+            var distance = g.ReduceSum(g.Abs(g.Add(trainingInput, g.Neg(xte))), axis: g.Const(1));
+
+            var pred = g.ArgMin(distance, g.Const(0));
+
+            var sess = new TFSession(g);
+
+            float accuracy = 0.0f;
+
+            for (int i = 0; i < testCount; i++)
+            {
+                var runner = sess.GetRunner();
+
+                var result = runner.
+                    Fetch(pred).
+                    Fetch(distance).
+                    AddInput(trainingInput, trainingImages).
+                    AddInput(xte, Extract(testImages, i)).Run();
+                var r = result[0].GetValue();
+                var tr = result[1].GetValue();
+                var nn_index = (int)(long)result[0].GetValue();
+
+                Console.WriteLine($"Test {i}: Prediction: { ArgMax(trainingLabels, nn_index) } True class: { ArgMax(testLabels, i)} (nn_index= { nn_index })");
+                if (ArgMax(trainingLabels, nn_index) == ArgMax(testLabels, i))
+                    accuracy += 1f / testImages.Length;
+            }
+
+            Console.WriteLine("Accuracy: " + accuracy);
+        }
+
+        static int ArgMax(float[,] array, int idx)
+        {
+            float max = -1;
+            int maxIdx = -1;
+            var l = array.GetLength(1);
+            for (int i = 0; i < l; i++)
+                if (array[idx, i] > max)
+                {
+                    maxIdx = i;
+                    max = array[idx, i];
+                }
+            return maxIdx;
+        }
+
+        static public float[] Extract(float[,] array, int index)
+        {
+            var n = array.GetLength(1);
+            var ret = new float[n];
+
+            for (int i = 0; i < n; i++)
+                ret[i] = array[index, i];
+            return ret;
+        }
+    }
+}
