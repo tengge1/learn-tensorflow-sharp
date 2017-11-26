dea3d2d3b6c1f2594b71362e9c0f3fbef95f5a1a
liteng <930372551@qq.com>
2017-11-26 11:16:12 +0800

    p04_InitVariable 修复多个变量初始化异常的bug。

--- p04_InitVariable/Program.cs
+++ p04_InitVariable/Program.cs
@@ -17,29 +17,28 @@ namespace p04_InitVariable
             // 创建图
             var g = new TFGraph();
 
-            // 创建操作和值，用于进行变量初始化，输出变量的值
-            // 注意：操作和值都无法共用。否则，后面的操作和值会覆盖掉前面的操作和值
-            TFOperation aInit, bInit;
-            TFOutput aValue, bValue;
-
             // 创建状态，用于输出操作状态
             TFStatus status = new TFStatus();
 
             // 定义变量
-            var a = g.Variable(g.Const(1.0), out aInit, out aValue);
-            var b = g.Variable(g.Range(g.Const(1.0), g.Const(100.0)), out bInit, out bValue);
+            var a = g.VariableV2(TFShape.Scalar, TFDataType.Double);
+            var initA = g.Assign(a, g.Const(1.0));
+
+            var b = g.VariableV2(new TFShape(99), TFDataType.Int32);
+            var initB = g.Assign(b, g.Range(g.Const(1), g.Const(100)));
 
             // 创建会话
             var sess = new TFSession(g);
+            var runner = sess.GetRunner();
 
             // 初始化变量，并输出操作状态
-            sess.GetRunner().AddTarget(aInit, bInit).Run(status);
+            runner.AddTarget(initA.Operation, initB.Operation).Run(status);
             Console.WriteLine(status.StatusCode);
 
             // 并输出计算状态和计算结果
-            var result = sess.GetRunner().Fetch(aValue, bValue).Run();
+            var result = runner.Fetch(a, b).Run();
             Console.WriteLine(result[0].GetValue());
-            Console.WriteLine(string.Join(",", (double[])result[1].GetValue()));
+            Console.WriteLine(string.Join(",", (int[])result[1].GetValue()));
         }
     }
 }
