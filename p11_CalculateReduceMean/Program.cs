f16152a423c9e2049461a6b6365895e8d0b3dcb2
liteng <930372551@qq.com>
2017-11-27 08:01:45 +0800

    p11_CalculateReduceMean

---
+++ p11_CalculateReduceMean/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TensorFlow;
+
+namespace p11_CalculateReduceMean
+{
+    /// <summary>
+    /// 11 计算矩阵维度平均值
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 创建图
+            var g = new TFGraph();
+
+            // 输入矩阵
+            var x = g.Const(new double[,]
+            {
+                { 1, 2 },
+                { 3, 4 }
+            });
+
+            // 计算ReduceMean
+            var y = g.ReduceMean(x, axis: g.Const(1));
+
+            // 创建会话
+            var sess = new TFSession(g);
+
+            // 计算结果
+            var result = sess.GetRunner().Run(y);
+
+            // 输出结果
+            // axis为 0，第一维求平均值：2,3
+            // axis为 1，第二维求平均值：1.5,3.5
+            PrintDoubleArray((double[])result.GetValue());
+        }
+
+        /// <summary>
+        /// 输出数组
+        /// </summary>
+        /// <param name="array"></param>
+        static void PrintDoubleArray(double[] array)
+        {
+            for (var i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + ",");
+            }
+            Console.WriteLine();
+        }
+    }
+}
