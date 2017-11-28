7017b1bbaa6b04fc62c486a2563f2cf2adc932a0
liteng <930372551@qq.com>
2017-11-28 13:01:41 +0800

    p15_PartialRun

---
+++ p15_PartialRun/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TensorFlow;
+
+namespace p15_PartialRun
+{
+    /// <summary>
+    /// 15 部分运行
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var g = new TFGraph();
+
+
+        }
+    }
+}
