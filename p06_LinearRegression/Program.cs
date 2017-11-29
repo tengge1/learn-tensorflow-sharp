cc0729627c28e8b11094bbe3d5fe793955b4fa2a
liteng <930372551@qq.com>
2017-11-29 21:04:27 +0800

    p06_LinearRegression 优化

--- p06_LinearRegression/Program.cs
+++ p06_LinearRegression/Program.cs
@@ -36,7 +36,7 @@ namespace p03_LinearFit
             var W = g.VariableV2(TFShape.Scalar, TFDataType.Double, operName: "weight");
             var b = g.VariableV2(TFShape.Scalar, TFDataType.Double, operName: "bias");
 
-            var initW = g.Assign(W, g.Const(1.0));
+            var initW = g.Assign(W, g.Const(100.0));
             var initb = g.Assign(b, g.Const(0.01));
 
             var output = g.Add(g.Mul(x, W), b);
@@ -44,7 +44,7 @@ namespace p03_LinearFit
             // 损失
             var loss = g.ReduceSum(g.Abs(g.Sub(output, y)));
             var grad = g.AddGradients(new TFOutput[] { loss }, new TFOutput[] { W, b });
-            var minimize = new[]
+            var optimize = new[]
             {
                 g.AssignSub(W, g.Mul(grad[0], g.Const(learning_rate))).Operation,
                 g.AssignSub(b, g.Mul(grad[1], g.Const(learning_rate))).Operation
@@ -61,8 +61,7 @@ namespace p03_LinearFit
             // 进行训练拟合
             for (var i = 0; i < 10; i++)
             {
-                var result = runner.Fetch(loss, W, b).Run();
-                runner.AddTarget(minimize).Run();
+                var result = runner.AddTarget(optimize).Fetch(loss, W, b).Run();
 
                 Console.WriteLine("loss: {0} W:{1} b:{2}", result[0].GetValue(), result[1].GetValue(), result[2].GetValue());
             }
