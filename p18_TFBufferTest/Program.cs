d09e92eefba3e93833879fbca21ee845e8f94b25
liteng <930372551@qq.com>
2017-12-02 21:38:53 +0800

    p18_TFBufferTest

---
+++ p18_TFBufferTest/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TensorFlow;
+
+namespace p18_TFBufferTest
+{
+    /// <summary>
+    /// 18 TFBuffer测试
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var hello = Encoding.UTF8.GetBytes("Hello, world!");
+
+            var buffer = new TFBuffer(hello);
+
+            var bytes = buffer.ToArray();
+
+            Console.WriteLine(Encoding.UTF8.GetString(bytes));
+        }
+    }
+}
