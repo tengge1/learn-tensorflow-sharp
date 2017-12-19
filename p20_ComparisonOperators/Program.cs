0a50a3d9592011504debeb3f92a592b5c888a5c6
liteng <930372551@qq.com>
2017-12-19 12:06:38 +0800

    add p20_ComparisonOperators

---
+++ p20_ComparisonOperators/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TensorFlow;
+
+namespace p20_ComparisonOperators
+{
+    /// <summary>
+    /// 20 Comparison Operators
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var g = new TFGraph();
+
+            var greater = g.Greater(g.Const(1), g.Const(0));
+            var greaterEqual = g.GreaterEqual(g.Const(1), g.Const(0));
+            var less = g.Less(g.Const(1), g.Const(2));
+            var lessEqual = g.LessEqual(g.Const(1), g.Const(1));
+
+            var sess = new TFSession(g);
+
+            var result = sess.GetRunner().Fetch(greater, greaterEqual, less, lessEqual).Run();
+
+            Console.WriteLine("{0} {1} {2} {3}", result[0].GetValue(), result[1].GetValue(), result[2].GetValue(), result[3].GetValue());
+        }
+    }
+}
